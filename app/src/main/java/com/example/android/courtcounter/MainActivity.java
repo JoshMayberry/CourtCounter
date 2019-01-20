7298e68e70afaaeb18a35003f9d00659f696c8b3
HOME\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2019-01-20 18:26:20 -0500

    Added Team B and a Reset Button bugfix for switch-case in onTeamA_addPoints()
    https://classroom.udacity.com/nanodegrees/nd803/parts/686861db-d0bf-4fe2-ab57-287444412d3f/modules/434cf327-f998-4e1d-8d7a-04ed96c43872/lessons/4036438656/concepts/43910585510923
    https://classroom.udacity.com/nanodegrees/nd803/parts/686861db-d0bf-4fe2-ab57-287444412d3f/modules/434cf327-f998-4e1d-8d7a-04ed96c43872/lessons/4036438656/concepts/43977585450923

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,15 +8,28 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int teamA_score = 0;
+    int teamB_score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayForTeamA(teamA_score);
+        displayForTeamA(teamB_score);
     }
 
-    /**Adds the appropriate amount to the score
+    /**Resets the scores.
+     *
+     * Use For: Button Event
+     */
+    public void onReset(View view) {
+        teamA_score = 0;
+        teamB_score = 0;
+        displayForTeamA(teamA_score);
+        displayForTeamB(teamB_score);
+    }
+
+    /**Adds the appropriate amount to the score for Team A.
      *
      * Use For: Button Event
      */
@@ -24,12 +37,34 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             case (R.id.teamA_add3):
                 teamA_score += 3;
+                break;
             case (R.id.teamA_add2):
                 teamA_score += 2;
+                break;
             case (R.id.teamA_add1):
                 teamA_score += 1;
+                break;
         }
         displayForTeamA(teamA_score);
+    }
+
+    /**Adds the appropriate amount to the score for Team B.
+     *
+     * Use For: Button Event
+     */
+    public void onTeamB_addPoints(View view) {
+        switch (view.getId()) {
+            case (R.id.teamB_add3):
+                teamB_score += 3;
+                break;
+            case (R.id.teamB_add2):
+                teamB_score += 2;
+                break;
+            case (R.id.teamB_add1):
+                teamB_score += 1;
+                break;
+        }
+        displayForTeamB(teamB_score);
     }
 
     /**Displays the given score for Team A.
@@ -40,6 +75,17 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.teamA_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**Displays the given score for Team B.
+     *
+     * @param score - What score the team has
+     *
+     * Example Input: displayForTeamB(10)
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.teamB_score);
         scoreView.setText(String.valueOf(score));
     }
 }
