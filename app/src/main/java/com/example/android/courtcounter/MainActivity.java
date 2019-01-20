eb81d09fe23607a847f533c6a9b10c49ccf516bc
HOME\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2019-01-20 17:32:25 -0500

    Instead of making a different function for each button, I used a switch case statement.
    https://classroom.udacity.com/nanodegrees/nd803/parts/686861db-d0bf-4fe2-ab57-287444412d3f/modules/434cf327-f998-4e1d-8d7a-04ed96c43872/lessons/4036438656/concepts/43888785510923

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int teamA_score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(teamA_score);
+    }
+
+    /**Adds the appropriate amount to the score
+     *
+     * Use For: Button Event
+     */
+    public void onTeamA_addPoints(View view) {
+        switch (view.getId()) {
+            case (R.id.teamA_add3):
+                teamA_score += 3;
+            case (R.id.teamA_add2):
+                teamA_score += 2;
+            case (R.id.teamA_add1):
+                teamA_score += 1;
+        }
+        displayForTeamA(teamA_score);
+    }
+
+    /**Displays the given score for Team A.
+     *
+     * @param score - What score the team has
+     *
+     * Example Input: displayForTeamA(10)
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.teamA_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
